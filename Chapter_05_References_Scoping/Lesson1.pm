7404fb366acb183d9f4f2ec41df2655d4f647bfc
MrSpock1994 <willpokerpro2016@gmail.com>
2022-10-06 19:44:36 -0300

    Chapter 5 about references

--- Chapter_05_References_Scoping/Lesson1.pm
+++ Chapter_05_References_Scoping/Lesson1.pm
@@ -16,10 +16,10 @@ my $ref_to_skipper = \@skipper;
 my $second_ref_to_skipper = $reference_to_skipper;
 my $third_ref_to_skipper = \@skipper;
 # At this point, we have four different ways to access the data contained in @skipper:
-@skipper
-@$ref_to_skipper
-@$second_ref_to_skipper
-@$third_ref_to_skipper
+# @skipper
+# @$ref_to_skipper
+# @$second_ref_to_skipper
+# @$third_ref_to_skipper
 
 # sub check_provisions_list(\@skipper)
 # When this subroutine executes, Perl creates a fifth reference to the data and copies it
@@ -29,3 +29,58 @@ my $third_ref_to_skipper = \@skipper;
 # We can kill off each reference by using the variable for something other than a reference
 # to the value of @skipper. For example, we can assign undef to the variable:
 $ref_to_skipper = undef;
+
+
+# What If That Was the Name?
+# Typically, all references to a variable are gone before the variable itself. But what if one
+# of the references outlives the variable name? For example, consider this code:
+
+my $ref;
+{
+    my @skipper = qw(blue_shirt hat jacket preserver sunscreen);
+    $ref = \@skipper;
+    print "$ref->[2]\n";
+}
+
+ print "$ref->[2]\n";
+
+# We can reduce the namespace clutter by narrowing down the scope of the various array
+# names. Rather than declaring them within the scope of the subroutine, we can create
+# a temporary block:
+
+my $skipper_with_name;
+{
+    my @skipper = qw(blue_shirt hat jacket preserver sunscreen);
+    @skipper_with_name = ('The Skipper', \@skipper);
+}
+
+print "@skipper_with_name[1]";
+
+# At this point, the second element of @skipper_with_name is a reference to the array
+# formerly known as @skipper. However, that name is no longer relevant.
+# This is a lot of typing to say “the second element should be a reference to an array
+# containing these elements.” We can create such a value directly using the anonymous
+# array constructor, which is yet another use for square brackets:
+# my $ref_to_skipper_provisions =
+# [ qw(blue_shirt hat jacket preserver sunscreen) ];
+# The square brackets take the value within (evaluated in list context); establish a new,
+# anonymous array initialized to those values; and (here’s the important part) return a
+# reference to that array. It’s as if we had said:
+# my $ref_to_skipper_provisions;
+# {
+# my @temporary_name =
+# ( qw(blue_shirt hat jacket preserver sunscreen) );
+# $ref_to_skipper_provisions = \@temporary_name;
+# }
+# We don’t need to come up with a temporary name, and we don’t need the extra noise
+# of the temporary block. The result of a square-bracketed anonymous array constructor
+# is an array reference, which fits wherever a scalar variable fits.
+
+# So, in summary, if we have this syntax:
+# my $fruits;
+# {
+# my @secret_variable = ('pineapple', 'papaya', 'mango');
+# $fruits = \@secret_variable;
+# }
+# we can replace it with:
+# my $fruits = ['pineapple', 'papaya', 'mango'];
